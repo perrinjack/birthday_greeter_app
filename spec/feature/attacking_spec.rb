78e5b997009be57877e36fa11a97bf33ad000e10
J Perrin <perrinjack96@gmail.com>
2020-05-29 14:48:13 +0100

    page displays player 2 HP after being attacked

--- spec/feature/attacking_spec.rb
+++ spec/feature/attacking_spec.rb
@@ -4,6 +4,12 @@ feature "attacking" do
   scenario "attacking confirmed" do
     sign_in_process
     click_button("attack")
-    expect(page).to have_content("player 1 got attacked")
+    expect(page).to have_content("player 2 got attacked")
+  end
+
+  scenario "reduces and displays player 2 HP" do
+    sign_in_process
+    click_button("attack")
+    expect(page).to have_content("player 2 40 HP")
   end
 end
