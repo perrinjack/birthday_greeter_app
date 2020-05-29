7ad1f0a629608fe666dcee7eb56b740b9f77f198
Rachael <rachaelewins@hotmail.com>
2020-05-29 14:29:16 +0100

    Player class, #name, returns @name

---
+++ spec/player_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/player.rb'
+
+describe Player do
+
+  describe "#name" do
+    let(:subject) {Player.new("name1")}
+    it "returns @name" do
+      expect(subject.name).to eq("name1")
+    end
+  end
+
+end
