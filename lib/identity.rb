d0523f8ee8cb99e88d898638011d6926ddbbec0b
J Perrin <perrinjack96@gmail.com>
2020-06-05 10:17:44 +0100

    Linter used

--- lib/identity.rb
+++ lib/identity.rb
@@ -7,7 +7,7 @@ class Identity
 
   def initialize(name, day, month)
     @name = name
-    @birthday = [day, month]
+    @birthday = day + month
     @birthday_message = "Happy Birthday #{name}! "
   end
 
@@ -16,16 +16,19 @@ class Identity
     birthday? ? @birthday_message : "Your birthday is in #{@days_until_birthday} days!"
   end
 
-     private
+  private
 
   def calculate_days
-    @current_date = Date.today
-    @birthday_date = Date.parse(@birthday.join(' ').to_s)
+    @current_date, @birthday_date = retrieve_date_formatting
     @birthday_date += 365 if birthday_date < current_date
     (birthday_date - current_date).to_i
+  end
+
+  def retrieve_date_formatting
+    [Date.today, Date.parse(birthday)]
   end
 
   def birthday?
     current_date == birthday_date
   end
-   end
+end
